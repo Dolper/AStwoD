b0ef2b4267d8b9c52c6d2e7116cfecff57fc8620
Демьян <d.e.m.a@bk.ru>
2013-06-08 11:51:50 +0400

    Revert "Connected DataBase and used EF"
    This reverts commit 02917c29ccc2053af8a147d29004ccc8def87aa2.

--- AStwoD-Project/AStwoD/Controllers/ControlPanelController.cs
+++ AStwoD-Project/AStwoD/Controllers/ControlPanelController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AStwoD.Models;
+using Newtonsoft.Json;
 
 namespace AStwoD.Controllers
 {
@@ -15,6 +17,47 @@ namespace AStwoD.Controllers
         {
             return View();
         }
+
+        //
+        // GET: /ControlPanel/AddPage
+
+        public ActionResult Page()
+        {
+            return View();
+        }
+
+
+        //
+        // POST: /ControlPanel/AddPage
+
+        [HttpPost]
+        public ActionResult Page(Page page)
+        {
+            throw new Exception("Создана страница: "+page.Title);
+            return View();
+        }
+
+        //
+        // GET: /ControlPanel/Pages
+
+        public ActionResult Pages()
+        {
+            if (Request.IsAjaxRequest())
+            {
+                var list = new List<Page>();
+                list.Add(new Page("Opa", "opopo", 1, "oooooo"));
+                list.Add(new Page("Opa2", "opopo2", 2, "oooooo"));
+                list.Add(new Page("Opa3", "opopo3", 3, "oooooo"));
+
+                return Json(list, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
 
         //
         // GET: /ControlPanel/Details/5
--- AStwoD-Project/AStwoD/Controllers/HomeController.cs
+++ AStwoD-Project/AStwoD/Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using AStwoD.Models;
 
 namespace AStwoD.Controllers
 {
