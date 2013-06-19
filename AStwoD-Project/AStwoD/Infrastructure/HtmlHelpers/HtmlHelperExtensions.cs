307e88880c64c527256135cae867fd4186d5f779
Denis Chudinov <dess-ase@mail.ru>
2013-06-19 09:24:09 +0400

    Revert "prepare to set <<>> -  @Html"
    This reverts commit bfdb52fee7625770aacbcf38936b10c0f87394db.

--- AStwoD-Project/AStwoD/Infrastructure/HtmlHelpers/HtmlHelperExtensions.cs
+++ AStwoD-Project/AStwoD/Infrastructure/HtmlHelpers/HtmlHelperExtensions.cs
@@ -1,27 +1,16 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using AStwoD.DAL.Repositories;
 
 namespace AStwoD.Infrastructure.HtmlHelpers
 {
     public static class HtmlHelperExtensions
     {
+        private static ComponentRepository componentRepository = new ComponentRepository();
         public static MvcHtmlString GetComponent(this HtmlHelper helper, string componentName)
         {
-            ComponentRepository componentRepository = new ComponentRepository();
             if (componentRepository.GetComponentByName(componentName) != null)
                 return new MvcHtmlString(componentRepository.GetComponentByName(componentName).Content);
             return new MvcHtmlString("has not contain :(");
         }
-
-        public static IEnumerable<int> GetAllIndexes(this string source, string matchString)
-        {
-            matchString = Regex.Escape(matchString);
-            return from Match match in Regex.Matches(source, matchString) select match.Index;
-        }
     }
-
-
 }
